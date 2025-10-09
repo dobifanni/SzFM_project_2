cd05a3b2e9942b19aada36c147291e3bbf990641
Ujj Ádám <adamujj66@mailbox.unideb.hu>
2025-10-09 12:39:02 +0200

    Alap kártya kinézet létrehozása
    A kártya látható elemeihez tartozó script hozzáadása

---
+++ Assets/Scripts/View/CardView.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text health;
+    [SerializeField] private TMP_Text damage;
+    [SerializeField] private SpriteRenderer imageSR;
+    [SerializeField] private GameObject wrapper;
+}
