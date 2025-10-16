c3d03a0a464c9c4d3c6566e9b9568777406f1a55
dobifanni <dobifanni@mailbox.unideb.hu>
2025-10-16 21:16:03 +0200

    Kártya system hozzáadása
    Card, CardData, CardView, Singleton

--- Assets/Scripts/View/CardView.cs
+++ Assets/Scripts/View/CardView.cs
@@ -1,10 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SocialPlatforms;
 
 public class CardView : MonoBehaviour
 {
     [SerializeField] private TMP_Text health;
     [SerializeField] private TMP_Text damage;
+    [SerializeField] private TMP_Text speed;
     [SerializeField] private SpriteRenderer imageSR;
     [SerializeField] private GameObject wrapper;
+
 }
